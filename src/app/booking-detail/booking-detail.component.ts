a8910274ec524287c1ee01a9c1079c9921901749
jerry19857 <piyapongvisitsin@gmail.com>
2022-05-12 09:41:34 +0700

    refactor: organize import

--- src/app/booking-detail/booking-detail.component.ts
+++ src/app/booking-detail/booking-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  FormArray, FormControl,
+  FormControl,
   FormGroup
 } from '@angular/forms';
 import { BookingService } from '../booking.service';
