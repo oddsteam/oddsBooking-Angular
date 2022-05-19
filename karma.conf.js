4edec9baa8cedc8e9f867c314f3a1e8f0c848bd8
jerry19857 <piyapongvisitsin@gmail.com>
2022-05-19 13:14:05 +0700

    edit test karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -41,18 +40,6 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: true,
-    restartOnFileChange: true,
-    customLaunchers: {
-      ChromeHeadless: {
-        base: "Chrome",
-        flags: [
-          "--headless",
-          "--disable-gpu",
-          "--no-sandbox",
-          "--remote-debugging-port=9222"
-        ],
-        
-      },
-    }
+    restartOnFileChange: true
   });
 };
