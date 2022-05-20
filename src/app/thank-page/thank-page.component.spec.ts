f719e0e7177d8491851af2a30ec6ba7d7cbe91ff
sittidetP <pawutinan_s@silpakorn.edu>
2022-05-20 14:55:34 +0700

    [jjs] fix: test ThankPageComponent

--- src/app/thank-page/thank-page.component.spec.ts
+++ src/app/thank-page/thank-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterModule } from '@angular/router';
 import { ThankPageComponent } from './thank-page.component';
 
 describe('ThankPageComponent', () => {
@@ -8,6 +9,7 @@ describe('ThankPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterModule.forRoot([])],
       declarations: [ ThankPageComponent ]
     })
     .compileComponents();
