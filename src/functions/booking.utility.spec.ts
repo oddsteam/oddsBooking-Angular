f1b15e756cf158b062ea718226706a1d2b97af24
Jirawat-rackz <Jirawat_ku@kkumail.com>
2022-06-21 13:46:13 +0700

    [porz] refactoring timeOption

--- src/functions/booking.utility.spec.ts
+++ src/functions/booking.utility.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing'
-import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { BookingUtility } from './booking.utility'
 import * as dayjs from 'dayjs'
 
@@ -12,15 +11,15 @@ describe('BookingUtility', () => {
 
     it('startTimeOption input[startDate(weekday)] #range should return [18:00,18:30,...,21:30,22:00]', () => {
         const expectedResult = [
-            { time: '18:00', duration: '0.5 hr' },
-            { time: '18:30', duration: '1 hr' },
-            { time: '19:00', duration: '1.5 hrs' },
-            { time: '19:30', duration: '2 hrs' },
-            { time: '20:00', duration: '2.5 hrs' },
-            { time: '20:30', duration: '3 hrs' },
-            { time: '21:00', duration: '3.5 hrs' },
-            { time: '21:30', duration: '4 hrs' },
-            { time: '22:00', duration: '4.5 hrs' },
+            { time: '18:00', duration: '0 hr' },
+            { time: '18:30', duration: '0 hr' },
+            { time: '19:00', duration: '0 hr' },
+            { time: '19:30', duration: '0 hr' },
+            { time: '20:00', duration: '0 hr' },
+            { time: '20:30', duration: '0 hr' },
+            { time: '21:00', duration: '0 hr' },
+            { time: '21:30', duration: '0 hr' },
+            { time: '22:00', duration: '0 hr' },
         ]
         const startDate = dayjs('6 Jul 2022').toDate()
         const result = BookingUtility.timeOption(startDate)
@@ -30,29 +29,29 @@ describe('BookingUtility', () => {
 
     it('startTimeOption input[startDate(weekend)] #range should return [9:00,9:30,...,19:30,20:00]', () => {
         const expectedResult = [
-            { time:'9:00', duration: '0.5 hr' },
-            { time:'9:30', duration: '1 hr' },
-            { time:'10:00', duration: '1.5 hrs' },
-            { time:'10:30', duration: '2 hrs' },
-            { time:'11:00', duration: '2.5 hrs' },
-            { time:'11:30', duration: '3 hrs' },
-            { time:'12:00', duration: '3.5 hrs' },
-            { time:'12:30', duration: '4 hrs' },
-            { time:'13:00', duration: '4.5 hrs' },
-            { time:'13:30', duration: '5 hrs' },
-            { time:'14:00', duration: '5.5 hrs' },
-            { time:'14:30', duration: '6 hrs' },
-            { time:'15:00', duration: '6.5 hrs' },
-            { time:'15:30', duration: '7 hrs' },
-            { time:'16:00', duration: '7.5 hrs' },
-            { time:'16:30', duration: '8 hrs' },
-            { time:'17:00', duration: '8.5 hrs' },
-            { time:'17:30', duration: '9 hrs' },
-            { time:'18:00', duration: '9.5 hrs' },
-            { time:'18:30', duration: '10 hrs' },
-            { time:'19:00', duration: '10.5 hrs' },
-            { time:'19:30', duration: '11 hrs' },
-            { time:'20:00', duration: '11.5 hrs' }
+            { time: '9:00', duration: '0 hr' },
+            { time: '9:30', duration: '0 hr' },
+            { time: '10:00', duration: '0 hr' },
+            { time: '10:30', duration: '0 hr' },
+            { time: '11:00', duration: '0 hr' },
+            { time: '11:30', duration: '0 hr' },
+            { time: '12:00', duration: '0 hr' },
+            { time: '12:30', duration: '0 hr' },
+            { time: '13:00', duration: '0 hr' },
+            { time: '13:30', duration: '0 hr' },
+            { time: '14:00', duration: '0 hr' },
+            { time: '14:30', duration: '0 hr' },
+            { time: '15:00', duration: '0 hr' },
+            { time: '15:30', duration: '0 hr' },
+            { time: '16:00', duration: '0 hr' },
+            { time: '16:30', duration: '0 hr' },
+            { time: '17:00', duration: '0 hr' },
+            { time: '17:30', duration: '0 hr' },
+            { time: '18:00', duration: '0 hr' },
+            { time: '18:30', duration: '0 hr' },
+            { time: '19:00', duration: '0 hr' },
+            { time: '19:30', duration: '0 hr' },
+            { time: '20:00', duration: '0 hr' },
         ]
         const startDate = dayjs('25 Jun 2022').toDate()
         const result = BookingUtility.timeOption(startDate)
@@ -81,7 +80,7 @@ describe('BookingUtility', () => {
             { time: '19:30', duration: '9 hrs' },
             { time: '20:00', duration: '9.5 hrs' },
             { time: '20:30', duration: '10 hrs' },
-            { time: '21:00', duration: '10.5 hrs' }
+            { time: '21:00', duration: '10.5 hrs' },
         ]
         const startDate = dayjs('25 Jun 2022').toDate()
         const startTime = '10:30'
@@ -116,7 +115,7 @@ describe('BookingUtility', () => {
             { time: '19:30', duration: '21 hrs' },
             { time: '20:00', duration: '21.5 hrs' },
             { time: '20:30', duration: '22 hrs' },
-            { time: '21:00', duration: '22.5 hrs' }
+            { time: '21:00', duration: '22.5 hrs' },
         ]
         const startDate = dayjs('25 Jun 2022').toDate()
         const startTime = '10:30'
--- src/functions/booking.utility.ts
+++ src/functions/booking.utility.ts
@@ -1,6 +1,7 @@
 import * as dayjs from 'dayjs'
 import { BookingService } from 'src/app/booking.service'
 
+type TimeOptionResponse = { time: string; duration: string }[]
 export class BookingUtility {
     constructor() {}
     static getEnableHours(startDate: Date, startTime: Date, endDate: Date): number[] {
@@ -59,91 +60,50 @@ export class BookingUtility {
     private static getTimeRange(startDate: Date, startHour?: number, endDate?: Date): number[] {
         if (startHour) {
             if (BookingService.isWeekend(startDate)) {
-                if (!dayjs(startDate).isSame(endDate)) {
-                    return BookingService.range(9, 21)
-                }
+                if (!dayjs(startDate).isSame(endDate)) return BookingService.range(9, 21)
                 return BookingService.range(startHour + 1, 21)
-            } else {
-                return BookingService.range(startHour + 1, 23)
             }
+            return BookingService.range(startHour + 1, 23)
         } else {
-            if (BookingService.isWeekend(startDate)) {
-                return BookingService.range(9, 20)
-            }
+            if (BookingService.isWeekend(startDate)) return BookingService.range(9, 20)
             return BookingService.range(18, 22)
         }
     }
 
     //เขียน test --> เช็คขอบบนขอบล่าง --> cover case? : เขียนแบบ map
     //ปรับเวลาวันอาทิตย์ 9:30
-    static timeOption(
-        startDate: Date,
-        time?: string,
-        endDate?: Date
-    ): { time: string; duration: string }[] {
+
+    static timeOption(startDate: Date, time?: string, endDate?: Date): TimeOptionResponse {
         const hours = Number(time?.split(':')[0])
-        const minutes = Number(time?.split(':')[1])
+        let minutes = time ? Number(time?.split(':')[1]) : 0
         const timeRange = this.getTimeRange(startDate, hours, endDate)
-        let times: { time: string; duration: string }[] = []
-        let addHour = (dayjs(startDate).day() === 6 && dayjs(endDate).day() === 0) ? timeRange.length - timeRange.indexOf(hours) - 2 : 0
-        
+        const times: TimeOptionResponse = []
+        let endDateTime = dayjs(endDate).hour(hours).minute(minutes)
+        const isSameDay = dayjs(startDate).isSame(endDate, 'day')
         timeRange.forEach((hour, index) => {
-            if (
-                (index === 0 && minutes === 30) ||
-                (index === 0 && !dayjs(startDate).isSame(endDate) && dayjs(endDate).day() === 0)
-            ) {
+            // loop push 0 and 30 minutes with condition
+            ;[0, 30].forEach((minute) => {
+                let dateTime = dayjs(startDate).hour(hour).minute(minute)
+                // if select startDate have 30 minute then don't show push index 0 and 0 minute of timeRange
+                if (index === 0 && minutes === 30 && minute === 0) return
+                // if isSunday then don't show push index 0 and 0 minute of timeRange
+                if (!isSameDay && endDateTime.day() === 0 && minute === 0 && index === 0) return
+                // don't show the last index of timeRange
+                if (index === timeRange.length - 1 && minute === 30) return
                 times.push({
-                    time: hour.toString() + ':30',
-                    duration: this.getDuration(addHour, minutes, 30, index),
+                    time: dateTime.format('H:mm'),
+                    duration: this.getDuration(dateTime, endDateTime),
                 })
-            } else if (index === timeRange.length - 1) {
-                times.push({
-                    time: hour.toString() + ':00',
-                    duration: this.getDuration(addHour, minutes, 0, index),
-                })
-            } else {
-                times.push({
-                    time: hour.toString() + ':00',
-                    duration: this.getDuration(addHour, minutes, 0, index),
-                })
-                times.push({
-                    time: hour.toString() + ':30',
-                    duration: this.getDuration(addHour, minutes, 30, index),
-                })
-            }
+            })
         })
         return times
     }
 
-    static getDuration(addHour: number, startMin: number, minute: number, index: number): string {
-        if (startMin === 0) {
-            if (index === 0) {
-                return minute === 30
-                    ? this.getTimeUnit(index + 1.5 + addHour)
-                    : this.getTimeUnit(index + 1 + addHour)
-            } else {
-                return minute === 30
-                    ? this.getTimeUnit(index + 1.5 + addHour)
-                    : this.getTimeUnit(index + 1 + addHour)
-            }
-        } else {
-            if (index === 0) {
-                return minute === 30
-                    ? this.getTimeUnit(index + 1 + addHour)
-                    : this.getTimeUnit(index + 0.5 + addHour)
-            } else {
-                return minute === 30
-                    ? this.getTimeUnit(index + 1 + addHour)
-                    : this.getTimeUnit(index + 0.5 + addHour)
-            }
-        }
-    }
-
-    static getTimeUnit(duration: number): string {
-        if (duration > 1.0) {
-            return String(duration) + ' hrs'
-        } else {
-            return String(duration) + ' hr'
-        }
+    static getDuration(dateTime: dayjs.Dayjs, endDate: dayjs.Dayjs): string {
+        const isSameDay = dayjs(dateTime).isSame(endDate, 'day')
+        const different = dateTime.add(isSameDay ? 0 : 1, 'day').diff(endDate, 'minute', true) / 60
+        let duration = isSameDay ? different : different + 12
+        duration = isNaN(duration) ? 0 : duration
+        return `${duration} ${duration > 1.0 ? 'hrs' : 'hr'}`
     }
 }
